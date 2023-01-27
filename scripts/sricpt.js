9e8b943cefa1c666891f41d519557a12b20b8ff2
mateusAnjos <mateus.anjosdefaria@gmail.com>
2023-01-27 19:46:08 -0300

    Começando a trabalhar Media Queries

--- scripts/sricpt.js
+++ scripts/sricpt.js
@@ -13,3 +13,15 @@ document.querySelectorAll('.accordion-button').forEach(button =>{
         }
     })
 })
+
+//Burger Menu Logic
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
