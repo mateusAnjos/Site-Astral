e7af7c868296c7edf032eff8ddad0a9d1dd254f4
mateusAnjos <mateus.anjosdefaria@gmail.com>
2023-02-01 19:44:26 -0300

    wip media 375px

--- scripts/sricpt.js
+++ scripts/sricpt.js
@@ -20,8 +20,15 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", mobileMenu);
+// document.body.addEventListener('click', closeMenuAnywhere);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
+
+// function closeMenuAnywhere(){
+//         hamburger.classList.remove("active");
+//         navMenu.classList.remove("active");
+//         console.log("fechou")
+// } 
